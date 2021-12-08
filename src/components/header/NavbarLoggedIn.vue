eed60edce3dc26eaa36ac4485b315086c2011ce1
Thoria94 <kamilmuratyilmaz@gmail.com>
2021-12-08 19:23:44 +0300

    feat: i18n package installed

---
+++ src/components/header/NavbarLoggedIn.vue
@@ -0,0 +1,11 @@
+<template>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
