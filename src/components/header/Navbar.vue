d2f2439f6631e26f6c924e4b01c1e91796041d91
Thoria94 <kamilmuratyilmaz@gmail.com>
2021-12-08 18:58:42 +0300

    feat: packages installed

---
+++ src/components/header/Navbar.vue
@@ -0,0 +1,13 @@
+<template>
+  <b-navbar type="dark" variant="dark">
+    <b-navbar-brand href="#">NavBar</b-navbar-brand>
+  </b-navbar>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+};
+</script>
+
+<style></style>
