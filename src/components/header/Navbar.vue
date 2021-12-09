4bdad470f91b07085af4ee9682a44585bd387a64
Thoria94 <kamilmuratyilmaz@gmail.com>
2021-12-09 14:29:47 +0300

    feat: add vue i18n

--- src/components/header/Navbar.vue
+++ src/components/header/Navbar.vue
@@ -1,6 +1,12 @@
 <template>
   <b-navbar type="dark" variant="dark">
     <b-navbar-brand href="#">NavBar</b-navbar-brand>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-form>
+        <b-button class="mx-2 my-sm-0">Search</b-button>
+        <b-button class="mx-2 my-sm-0">Search</b-button>
+      </b-nav-form>
+    </b-navbar-nav>
   </b-navbar>
 </template>
 
