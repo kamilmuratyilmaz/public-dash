99447dbd4e74910eb4c816de0f66ab3e288cef3a
Thoria94 <kamilmuratyilmaz@gmail.com>
2021-12-09 20:54:45 +0300

    feat: add default page image

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,6 +5,13 @@
 
   <div class="login" v-else>
     <Navbar />
+    <b-img-lazy
+      center
+      class="mt-5"
+      src="@/assets/homepage_renewable.jpeg"
+      fluid
+      alt="Responsive image"
+    ></b-img-lazy>
   </div>
 </template>
 
