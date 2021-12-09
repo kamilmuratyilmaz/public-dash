ebe997b298e598ad05fd5bd00f26ffd5ea329b64
Thoria94 <kamilmuratyilmaz@gmail.com>
2021-12-09 20:22:40 +0300

    feat: add NavbarLoggedIn texts

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,27 @@
 <template>
-  <div class="home">
+  <div class="logout" v-if="user == 'login'">
+    <NavbarLoggedIn />
+  </div>
+
+  <div class="login" v-else>
     <Navbar />
   </div>
 </template>
 
 <script>
 import Navbar from "../components/header/Navbar";
+import NavbarLoggedIn from "../components/header/NavbarLoggedIn";
 
 export default {
   name: "Home",
   components: {
     Navbar,
+    NavbarLoggedIn,
+  },
+  data() {
+    return {
+      user: "login",
+    };
   },
 };
 </script>
